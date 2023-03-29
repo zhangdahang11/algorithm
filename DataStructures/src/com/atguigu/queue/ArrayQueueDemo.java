eca0f12f5d011a177475d1f575fb8991ec928f5a
zhangdahang <11715603+zhangdahang1998@user.noreply.gitee.com>
2023-03-29 21:22:06 +0800

    new begin

---
+++ DataStructures/src/com/atguigu/queue/ArrayQueueDemo.java
@@ -0,0 +1,128 @@
+package com.atguigu.queue;
+
+import java.util.Scanner;
+
+/**
+ * @author zdhstart
+ * @date 2022-08-2916:33
+ */
+public class ArrayQueueDemo {
+    public static void main(String[] args) {
+        //测试一把
+        //创建一个队列
+        ArrayQueue queue = new ArrayQueue(3);
+        char key = ' ';//接受用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop) {
+            System.out.println("s(show)，显示队列");
+            System.out.println("e(exit)，退出程序");
+            System.out.println("a(add)，添加数据");
+            System.out.println("g(get)，获取数据");
+            System.out.println("h(head)，显示队头的数据");
+            key = scanner.next().charAt(0); //接受一个字符
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g': //取出数据
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h': //查看队列头的数据
+                    try {
+                        int res = queue.headQueue();
+                        System.out.printf("队列头的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e': //退出
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出～～");
+    }
+}
+
+//使用数组模拟队列-编写一个ArrayQueue类
+class ArrayQueue {
+    private final int maxSize; //表示数组的最大容量
+    private final int[] arr; //该数组用于存放数据，模拟队列
+    private int front; //队列头
+    private int rear; //队列尾
+
+    //创建队列的构造器
+    public ArrayQueue(int arrMaxSize) {
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+        front = -1; //指向队列头部，分析出front指向队列头的前一个位置
+        rear = -1; //指向队列尾，指向队列尾的数据（即就是队列最后一个数据）
+    }
+
+    //判断队列是否满
+    public boolean isFull() {
+        return rear == maxSize - 1;
+    }
+
+    //判断队列是否为空
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    //添加数据到队列
+    public void addQueue(int n) {
+        //判断队列是否满
+        if (isFull()) {
+            System.out.println("队列满，不能加入数据～");
+            return;
+        }
+        rear++; //让rear 后移
+        arr[rear] = n;
+    }
+
+    //获取队列的数据，出队列
+    public int getQueue() {
+        //判断队列是否空
+        if (isEmpty()) {
+            //通过抛出异常
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        front++; //front后移
+        return arr[front];
+    }
+
+    //显示队列的所有数据
+    public void showQueue() {
+        //遍历
+        if (isEmpty()) {
+            System.out.println("队列空的，没有数据～");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        }
+    }
+
+    //显示队列的头数据，注意不是取出数据
+    public int headQueue() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列空的，没有数据～");
+        }
+        return arr[front + 1];
+    }
+}
---
+++ DataStructures/src/com/atguigu/sparsearray/SparseArray1.java
@@ -0,0 +1,89 @@
+package com.atguigu.sparsearray;
+
+/**
+ * ClassName: SparseArray1
+ * Package: com.atguigu.sparsearray
+ * Description:
+ *
+ * @Author zdhstart
+ * @Create 2022/11/25 20:55
+ * @Version 1.0
+ */
+public class SparseArray1 {
+    public static void main(String[] args) {
+        int[][] arr = new int[11][11];
+        arr[1][2] = 1;
+        arr[2][3] = 2;
+        arr[4][5] = 2;
+        /*for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }*/
+        System.out.println("原始的二维数组");
+        for (int[] row :
+                arr) {
+            for (int data :
+                    row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        //2.创建对应的稀疏数组
+        int[][] arr1 = new int[sum + 1][3];
+        //给稀疏数组赋值
+        arr1[0][0] = 11;
+        arr1[0][1] = 11;
+        arr1[0][2] = sum;
+
+        int count = 0;
+        //遍历二维数组
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != 0) {
+                    count++;
+                    arr1[count][0] = i;
+                    arr1[count][1] = j;
+                    arr1[count][2] = arr[i][j];
+                }
+            }
+        }
+
+        //输出稀疏数组
+        System.out.println();
+        System.out.println("得到的稀疏数组为～～～");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", arr1[i][0], arr1[i][1], arr1[i][2]);
+        }
+        System.out.println();
+
+        System.out.println("二维");
+        //变会二维数组
+        int[][] arr2 = new int[arr1[0][0]][arr1[0][1]];
+
+        for (int i = 1; i < arr1.length; i++) {
+            arr2[arr1[i][0]][arr1[i][1]] = arr1[i][2];
+        }
+
+        for (int[] row :
+                arr2) {
+            for (int data :
+                    row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+    }
+}
